a63779c56bffddf96efc4809a999b655d7e22399
Kalman Olah <hello@kalmanolah.net>
2012-06-20 20:50:44 +0200

    Remove comments

--- WinScrot.cs
+++ WinScrot.cs
@@ -9,23 +9,6 @@ using System.Text.RegularExpressions;
 
 namespace WinScrot
 {
-    /*
-     * WinScrot (.NET edition), by Kalman Olah
-     * 
-     * Usage: WinScrot [-d DELAY] [-c] [-q QUALITY] [-t THUMBSIZE] [FILENAME.EXT]
-     * 
-     *     -d INT       Delay before the screenshot is taken, in seconds. Default: 0. Min: 0. Optional.
-     *     -c           Displays a countdown when used with delay. Default: False. Optional.
-     *     -q INT       Quality of the screenshot, in percentages. Default: 75. Min: 1. Max: 100. Optional.
-     *                  NOTE: Differs depending on image format.
-     *     -t INT       Resolution of the screenshot, in percentages of full desktop size. Default: 100. Min: 1. Max: 100. Optional.
-     *     file.ext     Filename of the screenshot, relative or absolute. Allowed extensions: png, jpg, jpeg, gif, bmp. Optional.
-     *                  NOTE: DateTime substitutions can be used in the filename in the "$dt.<example>$" format.
-     *                  e.g.: 'WinScrot-$dt.dd.MM.yyyy--HH.mm.ss$.png' -> 'WinScrot-01.07.2012--17.48.53.png'
-     *                    
-     * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-     * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ .
-     */
     class WinScrot
     {
         static int varDelay = 0;
